c1b7f083cdc06acf9122d2e282e4a9f69c7a1e82
YKBI <54012343+YKBI@users.noreply.github.com>
2020-05-29 16:53:53 +0900

    output code update

--- NeoInven.sh
+++ NeoInven.sh
@@ -6,13 +6,14 @@ seq=$2
 Ht=$3
 outdir="$pdb"_"$seq"_"$Ht"
 time python peptide_docking/peptide_docking.py $pdb $seq 500 peptide_docking/DockingDB-0508.txt
-rm -rf "$outdir"/PDB_*/*.out
+
 time python peptide_docking/crebfa.py "$pdb"_"$seq"_"$Ht"
 
+rm -rf "$outdir"/PDB_*/*.out "$outdir"/pdbs
 time tar cf - $outdir |pbzip2 > "$outdir".tar.bz2
 ssh user1@10.1.5.9 "mkdir -p /arc10-2-03/200529_NeoInven/output/"
-ssh user1@10.1.5.9 "mkdir -p /arc10-2-03/200529_NeoInven/output_comp/"
-time scp "$outdir".ba2 user1@10.1.5.9:/arc10-2-03/200529_NeoInven/output_comp/
+ssh user1@10.1.5.9 "mkdir -p /arc10-2-06/200529_NeoInven/output_comp/"
+time scp "$outdir".tar.bz2 user1@10.1.5.9:/arc10-2-06/200529_NeoInven/output_comp/
 time scp "$outdir"/*matrix/*total.txt user1@10.1.5.9:/arc10-2-03/200529_NeoInven/output/
 
 
