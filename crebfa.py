485b6a87f2e8788d27b2bf7bcfadf584c395af56
YKBI <54012343+YKBI@users.noreply.github.com>
2020-05-29 15:58:00 +0900

    Add files via upload

--- crebfa.py
+++ crebfa.py
@@ -16,8 +16,7 @@ def ff(x):
 	df[3] = x + '_' + df[0].str.split('.').str[0]
 	pre_rmsd.append(df)
 	for i in ppp:
-		os.system('sed \'s/HETATM/ATOM  /\' ' + i.split('.')[0] + '_het.pdb > ../pdbs/' + str(x) + '_' + '_'.join(i.split('.')[:5]) + '_rev.pdb')
-		#os.system('cp ' + i + ' ../pdbs')
+		os.system('sed \'s/HETATM/ATOM  /\' ' + i.split('.')[0] + '_het.pdb > ../pdbs/' +  '_'.join(i.split('.')[:4]) + '.pdb')
 	os.chdir('../')
 
 def pred(x):
@@ -29,16 +28,12 @@ def b_part(x):
 		pdbn = pdbid + '_' + i.split('_')[0] + '_' + i.split('_')[6]
 		df = pd.read_csv(i,sep=',')
 		df_pep = df[df['Chain index'] == ' B']
-		#for j in df_pep['Residue index'].tolist():
-		#	ni.append('B_factor-P' + str(j))
 		df_pep['new_index'] = nni
 		df_pep_d = df_pep.drop(['Residue index','Chain index'],axis=1).set_index('new_index').T.rename(index={'Predicted MD fluctuation value' : pdbn})
 		bf.append(df_pep_d)
 	pd.concat(bf).reset_index().rename(columns={'index':'PDB'}).to_csv('total_bfact.txt',sep='\t',index=False)
 	os.system('cp total_bfact.txt ../' + RES + '_energy_matrix/')
 	os.chdir('../')
-#ddf = os.getcwd()
-#os.system('cp --verbose -rf ' + RES + ' ~/')
 os.chdir('/home/user1/' + RES)
 if not os.path.exists('pdbs'):
 	os.mkdir('pdbs')
@@ -51,7 +46,7 @@ for nn in ['1ST','2ND']:
 	os.chdir('PDB_' + nn)
 	ff(nn)
 os.chdir('pdbs')
-pool = Pool(5)#psutil.cpu_count()-4)
+pool = Pool(psutil.cpu_count()-4)#psutil.cpu_count()-4)
 pool.map(pred,sorted(glob.glob('*.pdb')))
 pool.close()
 pool.join()
@@ -67,7 +62,7 @@ df_sk = pd.read_csv('total_sk.txt',sep='\t')
 df_hh = pd.read_csv('total_hh.txt',sep='\t')
 df_bf = pd.read_csv('total_bfact.txt',sep='\t')
 rmsd = pd.read_csv('colad_rmsd.txt',sep='\t')
-rmsd[4] = pdbid + '_' + rmsd['3'].str.split('_').str[0] + '_' + rmsd['3'].str.split('_').str[6]
+rmsd[4] = pdbid + '_' + rmsd['3'].str.split('_').str[0] + '_' + rmsd['3'].str.split('_').str[7]
 rmsd.columns = ['pdb','refer','rmsd','pre_PDB','PDB']
 rmsd.drop(['pdb','pre_PDB'],axis=1).set_index(['PDB']).reset_index().to_csv('total_rmsd1.txt',sep='\t',index=False)
 df_rmsd = pd.read_csv('total_rmsd1.txt',sep='\t').drop(['refer'],axis=1)
@@ -77,8 +72,5 @@ df_final1 = reduce(lambda left,right:pd.merge(left,right,on=['PDB'],how='outer')
 df_final2 = pd.merge(df_final1.set_index('rmsd').reset_index(),df_ac).dropna()
 df_final2['PDB1'] = df_final2['PDB'].str.split('_').str[0] + '_' + inseq + '_' + df_final2['PDB'].str.split('_').str[1] + '_' + df_final2['PDB'].str.split('_').str[2]
 df_final3 = df_final2.drop(['PDB'],axis=1).rename(columns={'PDB1':'PDB'}).set_index('PDB').reset_index()
-#df_final3['class'] = tf
 df_final3.to_csv(pdbid + '_' + inseq + '_' + hla +'_total.txt',sep='\t',index=False,na_rep='-')
 os.chdir('/home/user1/')
-#os.system('cp --verbose -rf ' + RES + ' ' + ddf)
-#os.system('rm -rf ' + RES)
